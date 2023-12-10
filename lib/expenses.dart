a2e7cb76973251b8bf19502ebeb056a433bb79eb
Alpesh Jamgade <alpesh@tradelab.in>
2023-12-10 07:26:54 +0530

    Initial code

---
+++ lib/expenses.dart
@@ -0,0 +1,49 @@
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _Expenses();
+  }
+}
+
+class _Expenses extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Burger',
+      amount: 5.0,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: 'Travel',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('The Chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
